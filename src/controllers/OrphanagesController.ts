96c260cfd82085faba6aa8e36bffd84d68e0f75d
Saulo Castilhos <saulo.castilhos@elevenfinancial.com>
2020-10-14 21:37:25 -0300

    fix: returnin images from db

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -7,7 +7,9 @@ export default {
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ["images"],
+    });
 
     return res.json(orphanages);
   },
@@ -17,7 +19,9 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ["images"],
+    });
 
     return res.json(orphanage);
   },
